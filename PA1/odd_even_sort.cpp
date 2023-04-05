61c5674f2685fa84f7dcdbb4f1cca15c2785bb37
tannlonggtaoo <tlt19@mails.tsinghua.edu.cn>
2023-04-05 21:48:43 +0800

    overlap version

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -6,7 +6,48 @@
 
 #include "worker.h"
 
-void Worker::sort() {
+/* bool dont_need_merge(int peerrank, float mymargin, int iter, int direction)
+{
+  // check margin to determine if merging is needed
+  float peermargin;
+  MPI_Status send_status, recv_status;
+  MPI_Request send_request, recv_request;
+  MPI_Irecv(
+      &peermargin,
+      1,
+      MPI_FLOAT,
+      peerrank,
+      iter,
+      MPI_COMM_WORLD,
+      &recv_request);
+  MPI_Isend(
+      &mymargin,
+      1,
+      MPI_FLOAT,
+      peerrank,
+      iter,
+      MPI_COMM_WORLD,
+      &send_request);
+
+  MPI_Wait(&send_request, &send_status);
+  MPI_Wait(&recv_request, &recv_status);
+
+  return (((direction == 1) && (mymargin <= peermargin)) 
+      || ((direction == -1) && (peermargin <= mymargin)));
+} */
+
+/* void printbuf(float* buf, int len, int rank, int iter)
+{
+  printf("[proc %d] iter %d :", rank, iter);
+  for (int i = 0; i < len; i++)
+  {
+    printf("%lf ",buf[i]);
+  }
+  printf("\n");
+} */
+
+void Worker::sort()
+{
   /** Your code ... */
   // you can use variables in class Worker: n, nprocs, rank, block_len, data
 
@@ -24,95 +65,128 @@ void Worker::sort() {
   std::sort(this->data, this->data + this->block_len);
 
   // STEP 1: pair-wise merging
-  MPI_Status send_status, recv_status;
-  MPI_Request send_request, recv_request;
+  MPI_Status send_status[2], recv_status[2];
+  MPI_Request send_request[2], recv_request[2];
   int comm_block_len = ceiling(this->n,this->nprocs);
-  float* mergebuf = new float[comm_block_len + this->block_len];
-  // std::merge is enough, do not use std::inplace_merge
+  float** mergebuf = new float* [2];
+  mergebuf[0] = new float[comm_block_len + this->block_len];
+  mergebuf[1] = new float[comm_block_len + this->block_len];
 
   int direction;
   int peerrank;
   int peerlen;
-  float peermargin;
-  float mymargin;
 
-  // TODO : try min-max first
   for (int i = 0; i < this->nprocs; i++)
   {
+
     direction = (i % 2 == 0 ? 1 : -1) * (this->rank % 2 == 0 ? 1 : -1);
     peerrank = this->rank + direction;
-
-    if ((peerrank < 0) || (comm_block_len * peerrank >= (int)this->n))
+    if (i == 0)
     {
-      continue;
+      // first iter, need to wait for the first data
+      if ((peerrank >= 0) && (comm_block_len * peerrank < (int)this->n))
+      {
+        MPI_Irecv(
+          mergebuf[i%2] + this->block_len,
+          comm_block_len,
+          MPI_FLOAT,
+          peerrank,
+          i,
+          MPI_COMM_WORLD,
+          &recv_request[i%2]);
+        MPI_Isend(
+          this->data,
+          this->block_len,
+          MPI_FLOAT,
+          peerrank,
+          i,
+          MPI_COMM_WORLD,
+          &send_request[i%2]);
+
+        printf("[proc %d] iter %d : <--n--> proc %d, tag %d @ idx %d\n",this->rank, i, peerrank, i, i%2);
+      }
+    } // first iter
+
+    // get next data without waiting
+    // last iter do not need to get next data
+    direction = -(i % 2 == 0 ? 1 : -1) * (this->rank % 2 == 0 ? 1 : -1);
+    peerrank = this->rank + direction;
+    if (i != (this->nprocs - 1))
+    {
+      if ((peerrank >= 0) && (comm_block_len * peerrank < (int)this->n))
+      {
+        MPI_Irecv(
+          mergebuf[(i+1)%2] + this->block_len,
+          comm_block_len,
+          MPI_FLOAT,
+          peerrank,
+          i + 1,
+          MPI_COMM_WORLD,
+          &recv_request[(i+1)%2]);
+        
+
+        printf("[proc %d] iter %d next : ready to recieve proc %d, tag %d @ idx %d\n",this->rank, i, peerrank, i + 1, (i+1)%2);
+
+      }
     }
 
-    // get margin num
-    MPI_Irecv(
-      &peermargin,
-      1,
-      MPI_FLOAT,
-      peerrank,
-      i,
-      MPI_COMM_WORLD,
-      &recv_request);
 
-    mymargin = this->data[(direction == 1) ? (this->block_len - 1) : 0];
-
-    MPI_Isend(
-      &mymargin,
-      1,
-      MPI_FLOAT,
-      peerrank,
-      i,
-      MPI_COMM_WORLD,
-      &send_request);
-
-    MPI_Wait(&recv_request, &recv_status);
-    MPI_Wait(&send_request, &send_status);
-
-    if (((direction == 1) && (mymargin <= peermargin)) 
-      || ((direction == -1) && (peermargin <= mymargin)))
+    // determine if we need merging at this iter
+    direction = (i % 2 == 0 ? 1 : -1) * (this->rank % 2 == 0 ? 1 : -1);
+    peerrank = this->rank + direction;
+    if ((peerrank >= 0) && (comm_block_len * peerrank < (int)this->n))
     {
-      continue;
+      MPI_Wait(&recv_request[i%2], &recv_status[i%2]);
+      MPI_Get_count(&recv_status[i%2], MPI_FLOAT, &peerlen);
+
+      std::merge(
+        this->data,
+        this->data + this->block_len,
+        mergebuf[i%2] + this->block_len,
+        mergebuf[i%2] + this->block_len + peerlen,
+        mergebuf[i%2]);
+      if (direction == 1)
+      {
+        memcpy(this->data, mergebuf[i%2], this->block_len * sizeof(float));
+      }
+      else // direction == -1
+      {
+        memcpy(this->data, mergebuf[i%2] + peerlen, this->block_len * sizeof(float));
+      }
+
+      printf("[proc %d] iter %d : merged @ idx %d\n",this->rank, i, i%2);
     }
 
-    // get all if needed
-    MPI_Irecv(
-      mergebuf + this->block_len,
-      comm_block_len,
-      MPI_FLOAT,
-      peerrank,
-      i,
-      MPI_COMM_WORLD,
-      &recv_request);
-    MPI_Isend(
-      this->data,
-      this->block_len,
-      MPI_FLOAT,
-      peerrank,
-      i,
-      MPI_COMM_WORLD,
-      &send_request);
-    MPI_Wait(&recv_request, &recv_status);
-    MPI_Get_count(&recv_status, MPI_FLOAT, &peerlen);
+    // send merged data
+    direction = -(i % 2 == 0 ? 1 : -1) * (this->rank % 2 == 0 ? 1 : -1);
+    peerrank = this->rank + direction;
 
-    std::merge(
-      this->data,
-      this->data + this->block_len,
-      mergebuf + this->block_len,
-      mergebuf + this->block_len + peerlen,
-      mergebuf);
-    if (direction == 1)
+    if (i != (this->nprocs - 1))
     {
-      memcpy(this->data, mergebuf, this->block_len * sizeof(float));
-    }
-    else // direction == -1
-    {
-      memcpy(this->data, mergebuf + peerlen, this->block_len * sizeof(float));
+      if ((peerrank >= 0) && (comm_block_len * peerrank < (int)this->n))
+      {
+        
+        MPI_Isend(
+          this->data,
+          this->block_len,
+          MPI_FLOAT,
+          peerrank,
+          i + 1,
+          MPI_COMM_WORLD,
+          &send_request[(i+1)%2]);
+
+          printf("[proc %d] iter %d next : --n--> proc %d, tag %d @ idx %d\n",this->rank, i, peerrank, i + 1, (i+1)%2);
+      }
     }
     // last step
-    MPI_Wait(&send_request, &send_status);
+    direction = (i % 2 == 0 ? 1 : -1) * (this->rank % 2 == 0 ? 1 : -1);
+    peerrank = this->rank + direction;
+    if ((peerrank >= 0) && (comm_block_len * peerrank < (int)this->n))
+    {
+      MPI_Wait(&send_request[i%2], &send_status[i%2]);
+    }
   }
+  delete [] mergebuf[0];
+  delete [] mergebuf[1];
   delete [] mergebuf;
 }
