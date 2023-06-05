c9de33b064d36e494fc01e4381f14f0ba60680c4
tannlonggtaoo <tlt19@mails.tsinghua.edu.cn>
2023-06-06 01:15:26 +0800

    first complete version

--- PA3/src/spmm_opt.cu
+++ PA3/src/spmm_opt.cu
@@ -1,74 +1,69 @@
+// reference : GE-SpMM (arXiv:2007.03179)
 #include "spmm_opt.h"
 
-constexpr int THRBLK_SIZE = 128; // thread block size
-constexpr int ROWBLK_SIZE = 32;  // #rows for each thread block
-constexpr int INFEATURE_MAX = 256;
+constexpr int BLOCK_SIZE = 32;
+constexpr int WARP_SIZE = 32;
 
 __global__ void spmm_kernel_placeholder(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int INFEATURE)
-{
-    // STEP 1 preparation
-    // params for the whole thread block
-    const int rblklo = blockIdx.x * ROWBLK_SIZE; // row block begin row
-    const int rblkhi = min((blockIdx.x + 1) * blockDim.x, num_v); // row block end row (not included)
-    const int valcnt = (ptr[rblkhi] - ptr[rblklo] + THRBLK_SIZE - 1) / THRBLK_SIZE; // len(ptr) = num_v + 1
-    __shared__ int ansbuf[ROWBLK_SIZE * INFEATURE_MAX]; // should use constexpr so define INFEATURE_MAX (assume INFEATURE <= 256)
-    // shared memory is NOT INITIALIZED
-    int cnt4eachthr = ROWBLK_SIZE * INFEATURE / THRBLK_SIZE;
-    for (int i = cnt4eachthr * threadIdx.x; i < cnt4eachthr * (threadIdx.x + 1); i++)
-    {
-        ansbuf[i] = 0;
-    }
-    __syncthreads();
-    // (segmentation here is the same as STEP 3)
+{	
+	const int rid = blockIdx.x * BLOCK_SIZE + threadIdx.y;
+	// each thread block handles BLOCK_SIZE rows
+	const int cid = blockIdx.y * WARP_SIZE + threadIdx.x;
+	// 0 to feat_in
+    const int tid = threadIdx.y * WARP_SIZE + threadIdx.x;	
+	// 0 to WARP_SIZE*BLOCK_SIZE
+	if (rid >= num_v) return;
 
-    // params for this thread
-    const int vallo = ptr[rblklo] + threadIdx.x * valcnt;
-    const int valhi = min(ptr[rblklo] + (threadIdx.x + 1) * valcnt, ptr[rblkhi]); // not included as well
-    
-    // find which row(s) this thread is handling
-    int rlo = rblklo;
-    while (ptr[rlo] <= vallo) rlo++;
-    rlo--;
-    int rhi = rlo;
-    while (ptr[rhi] <= valhi) rhi++; // rhi not included
+	extern __shared__ int sm[];
+	int* sm_k = sm;   									// for caching idx
+	float* sm_v = (float*)sm + BLOCK_SIZE * WARP_SIZE;  // for caching val
+	int sm_base = threadIdx.y * WARP_SIZE;				// 0,32,64,...
 
-    // STEP 2 computation
-    // all atomic
-    for (int r = rlo; r < rhi; r++)
-    {
-        // low efficiency
-        int ibegin = (r == rlo) ? vallo : ptr[r];
-        int iend = (r == (rhi - 1)) ? valhi : ptr[r+1];
-        for (int j = 0; j < INFEATURE; j++)
-        {
-            // j: col index of B
-            float result = 0.0f;
-            for (int i = ibegin; i < iend; i++)
-            {
-                result += vin[idx[i] * INFEATURE + j] * val[i];
-            }
-            // may try other scopes (now on device)
-            atomicAdd(ansbuf[r * INFEATURE_MAX + j], result);
-        }
-    }
-    __syncthreads();
+	int begin = ptr[rid], end = ptr[rid + 1];
+	float result = 0.0f;
+	int k;
 
-    // STEP 3 move to global memory
-    for (int i = cnt4eachthr * threadIdx.x; i < cnt4eachthr * (threadIdx.x + 1); i++)
-    {
-        vout[rblklo * INFEATURE + i] = ansbuf[i];
-    }
+	// iteration over whole row
+	for (int p = begin; p < end; p+=WARP_SIZE)
+	{
+		// loading A (caching)
+		if (p + threadIdx.x < end)
+		{
+			sm_k[tid] = idx[p + threadIdx.x];
+			sm_v[tid] = val[p + threadIdx.x];
+		}
+		__syncwarp();	// wait till all float num loaded
+		
+		// computation
+		for (int kk = 0; kk < WARP_SIZE; kk++)
+		{
+			if (p + kk < end)
+			{
+				k = sm_k[sm_base + kk];		// corresponding idx
+				if (cid < INFEATURE)
+				{
+					result += sm_v[sm_base + kk] * vin[k * INFEATURE + cid];
+				}
+			}
+		}
+		__syncwarp();
+	}
+	if (cid < INFEATURE)
+	{
+		vout[rid * INFEATURE + cid] = result;
+	}
 }
 void SpMMOpt::preprocess(float *vin, float *vout)
 {
-    grid.x = (num_v + ROWBLK_SIZE - 1) / ROWBLK_SIZE; // num_v = #rows in total
-    block.x = THRBLK_SIZE;
-    // equalization within each thread block (ROWBLK_SIZE rows)...
-
+    // TODO: your code
+    grid.x = (num_v + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	grid.y = (feat_in + WARP_SIZE - 1) / WARP_SIZE;
+    block.x = WARP_SIZE;
+	block.y = BLOCK_SIZE;
 }
 
 void SpMMOpt::run(float *vin, float *vout)
 {
     // TODO: your code
-    spmm_kernel_placeholder<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in);
+    spmm_kernel_placeholder<<<grid, block, WARP_SIZE*BLOCK_SIZE*(sizeof(int) + sizeof(float))>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in);
 }
